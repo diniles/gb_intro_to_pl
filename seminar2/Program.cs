38c68c9889743d62b5ecc94beb70477f98f0cae3
diniles <dzliashko@gmail.com>
2023-04-12 11:52:34 +0300

    Feat: task 13 finished

--- seminar2/Program.cs
+++ seminar2/Program.cs
@@ -5,6 +5,26 @@
 // 782 -> 8
 // 918 -> 1
 
+// Console.Write("Enter the number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(number % 100 / 10);
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или
+// сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
 Console.Write("Enter the number: ");
-int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(number % 100 / 10);
+string number = Console.ReadLine();
+if (number.Length < 3)
+{
+  Console.WriteLine("No third number!");
+}
+else
+{
+  Console.WriteLine(number[2]);
+}
