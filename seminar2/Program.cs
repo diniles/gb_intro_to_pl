d29a7f004f7636de3b6b506c11db91341cf64f7e
diniles <dzliashko@gmail.com>
2023-04-12 12:01:54 +0300

    Feat: task 15 finished

--- seminar2/Program.cs
+++ seminar2/Program.cs
@@ -18,13 +18,42 @@
 
 // 32679 -> 6
 
+// Console.Write("Enter the number: ");
+// string number = Console.ReadLine();
+// if (number.Length < 3)
+// {
+//   Console.WriteLine("No third number!");
+// }
+// else
+// {
+//   Console.WriteLine(number[2]);
+// }
+
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую
+// день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
 Console.Write("Enter the number: ");
-string number = Console.ReadLine();
-if (number.Length < 3)
+string dayNumber = Console.ReadLine();
+
+switch (dayNumber)
 {
-  Console.WriteLine("No third number!");
-}
-else
-{
-  Console.WriteLine(number[2]);
+  case "1":
+  case "2":
+  case "3":
+  case "4":
+  case "5":
+    Console.WriteLine("No");
+    break;
+  case "6":
+  case "7":
+    Console.WriteLine("Yes");
+    break;
+  default:
+    Console.WriteLine("Wrong day number!");
+    break;
 }
