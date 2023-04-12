c8741da2db2973e96128aa5a70874d68bb9c94ca
diniles <dzliashko@gmail.com>
2023-04-12 12:04:36 +0300

    Feat: all tasks finished

--- seminar2/Program.cs
+++ seminar2/Program.cs
@@ -37,23 +37,23 @@
 // 7 -> да
 // 1 -> нет
 
-Console.Write("Enter the number: ");
-string dayNumber = Console.ReadLine();
+// Console.Write("Enter the number: ");
+// string dayNumber = Console.ReadLine();
 
-switch (dayNumber)
-{
-  case "1":
-  case "2":
-  case "3":
-  case "4":
-  case "5":
-    Console.WriteLine("No");
-    break;
-  case "6":
-  case "7":
-    Console.WriteLine("Yes");
-    break;
-  default:
-    Console.WriteLine("Wrong day number!");
-    break;
-}
+// switch (dayNumber)
+// {
+//   case "1":
+//   case "2":
+//   case "3":
+//   case "4":
+//   case "5":
+//     Console.WriteLine("No");
+//     break;
+//   case "6":
+//   case "7":
+//     Console.WriteLine("Yes");
+//     break;
+//   default:
+//     Console.WriteLine("Wrong day number!");
+//     break;
+// }
