9dd7c17781ec7963f8ff31a1961de5294e9baf96
diniles <dzliashko@gmail.com>
2023-04-12 10:42:29 +0300

    Feat: task 4 solution

--- seminar1/Program.cs
+++ seminar1/Program.cs
@@ -5,26 +5,46 @@
 // a = 2 b = 10->max = 10
 // a = -9 b = -3->max = -3
 
-Console.Write("Enter first number: ");
-int firstNum = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter second number: ");
-int secondNum = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter first number: ");
+// int firstNum = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter second number: ");
+// int secondNum = Convert.ToInt32(Console.ReadLine());
 
-if (firstNum > secondNum)
-{
-  Console.WriteLine($"The first number {firstNum} is bigger than second number {secondNum}.");
-}
-else if (firstNum < secondNum)
-{
-  Console.WriteLine($"The second number {secondNum} is bigger than first number {firstNum}.");
-}
-else
-{
-  Console.WriteLine("The numbers are equal.");
-}
+// if (firstNum > secondNum)
+// {
+//   Console.WriteLine($"The first number {firstNum} is bigger than second number {secondNum}.");
+// }
+// else if (firstNum < secondNum)
+// {
+//   Console.WriteLine($"The second number {secondNum} is bigger than first number {firstNum}.");
+// }
+// else
+// {
+//   Console.WriteLine("The numbers are equal.");
+// }
 
 // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+
+//P.S. Я не рассматривал случаи, когда все или некоторые из чисел равны, но, если надо, могу переделать.
+Console.Write("Enter first number: ");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number: ");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter third number: ");
+int thirdNum = Convert.ToInt32(Console.ReadLine());
+if (firstNum > secondNum && firstNum > thirdNum)
+{
+  Console.WriteLine($"The first number {firstNum} is biggest.");
+}
+if (secondNum > firstNum && secondNum > thirdNum)
+{
+  Console.WriteLine($"The second number {secondNum} is biggest.");
+}
+if (thirdNum > firstNum && thirdNum > secondNum)
+{
+  Console.WriteLine($"The third number {thirdNum} is biggest.");
+}
