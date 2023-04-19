f2b8d5d921e7131374206ff012bf590fa1fcc286
diniles <dzliashko@gmail.com>
2023-04-19 16:21:51 +0300

    Feat: task 25 solved

--- seminar4/Program.cs
+++ seminar4/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и
+// возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4-> 16
+
+Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter a power: ");
+int power = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int result = 1;
+while (i <= power)
+{
+  result *= number;
+  i++;
+}
+Console.WriteLine(result);
