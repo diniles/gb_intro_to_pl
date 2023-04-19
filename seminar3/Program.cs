5f860976c8d70f6cfc96dd1c852e4b0dd5b3fb05
diniles <dzliashko@gmail.com>
2023-04-19 15:24:59 +0300

    Feat: task 19 solved

--- seminar3/Program.cs
+++ seminar3/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+Console.Write("Enter the number: ");
+string number = Console.ReadLine();
+
+char[] arrayOfNums = number.ToCharArray();
+Array.Reverse(arrayOfNums);
+string reversedNumber = new string(arrayOfNums);
+
+if (number == reversedNumber)
+{
+  Console.WriteLine("YES");
+}
+else
+{
+  Console.WriteLine("NO");
+}
