b82eee0dd304cfb1f2384e50e4c5b9f966d2632f
diniles <dzliashko@gmail.com>
2023-04-19 15:41:08 +0300

    Feat: task 23 solved

--- seminar3/Program.cs
+++ seminar3/Program.cs
@@ -36,21 +36,38 @@
 
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-Console.Write("Enter the first point X: ");
-int x1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter the first point Y: ");
-int y1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter the first point Z: ");
-int z1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter the second point X: ");
-int x2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter the second point Y: ");
-int y2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter the second point Z: ");
-int z2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter the first point X: ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter the first point Y: ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter the first point Z: ");
+// int z1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter the second point X: ");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter the second point Y: ");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter the second point Z: ");
+// int z2 = Convert.ToInt32(Console.ReadLine());
 
-double distX = Math.Pow((x2 - x1), 2);
-double distY = Math.Pow((y2 - y1), 2);
-double distZ = Math.Pow((z2 - z1), 2);
+// double distX = Math.Pow((x2 - x1), 2);
+// double distY = Math.Pow((y2 - y1), 2);
+// double distZ = Math.Pow((z2 - z1), 2);
 
-Console.WriteLine(Math.Sqrt(distX + distY + distZ));
+// Console.WriteLine(Math.Sqrt(distX + distY + distZ));
+
+// ==========================================================================
+
+// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Enter the number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= num; i++)
+{
+  Console.Write($"{Math.Pow(i, 3)} ");
+}
