fb6365f30af36147f4440fd3482bb1a2413c4af5
diniles <dzliashko@gmail.com>
2023-04-19 15:37:24 +0300

    Feat: task 21 solved

--- seminar3/Program.cs
+++ seminar3/Program.cs
@@ -9,18 +9,48 @@
 
 // 23432 -> да
 
-Console.Write("Enter the number: ");
-string number = Console.ReadLine();
+// Console.Write("Enter the number: ");
+// string number = Console.ReadLine();
 
-char[] arrayOfNums = number.ToCharArray();
-Array.Reverse(arrayOfNums);
-string reversedNumber = new string(arrayOfNums);
+// char[] arrayOfNums = number.ToCharArray();
+// Array.Reverse(arrayOfNums);
+// string reversedNumber = new string(arrayOfNums);
 
-if (number == reversedNumber)
-{
-  Console.WriteLine("YES");
-}
-else
-{
-  Console.WriteLine("NO");
-}
+// if (number == reversedNumber)
+// {
+//   Console.WriteLine("YES");
+// }
+// else
+// {
+//   Console.WriteLine("NO");
+// }
+
+// ==========================================================================
+
+// Задача 21
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит
+// расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Write("Enter the first point X: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the first point Y: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the first point Z: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the second point X: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the second point Y: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the second point Z: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double distX = Math.Pow((x2 - x1), 2);
+double distY = Math.Pow((y2 - y1), 2);
+double distZ = Math.Pow((z2 - z1), 2);
+
+Console.WriteLine(Math.Sqrt(distX + distY + distZ));
