65c259dc443e79bd5682e65cae2eddc5a1d80415
diniles <dzliashko@gmail.com>
2023-04-20 10:29:04 +0300

    Feat: task 27 solved

--- seminar4/Program.cs
+++ seminar4/Program.cs
@@ -5,15 +5,39 @@
 
 // 2, 4-> 16
 
+// Console.Write("Enter a number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter a power: ");
+// int power = Convert.ToInt32(Console.ReadLine());
+// int i = 1;
+// int result = 1;
+// while (i <= power)
+// {
+//   result *= number;
+//   i++;
+// }
+// Console.WriteLine(result);
+
+// ===============================================================================
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму
+// цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
 Console.Write("Enter a number: ");
 int number = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter a power: ");
-int power = Convert.ToInt32(Console.ReadLine());
-int i = 1;
-int result = 1;
-while (i <= power)
+
+int sum = 0;
+
+while (number > 0)
 {
-  result *= number;
-  i++;
+  sum += number % 10;
+  number /= 10;
 }
-Console.WriteLine(result);
+
+Console.WriteLine(sum);
