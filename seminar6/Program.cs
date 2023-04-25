a6049faa83aac6f382d7426d8885c13c3e7e2958
Dzmitry Liashko <dzliashko@gmail.com>
2023-04-25 22:49:38 +0300

    Feat: all tasks solved

--- seminar6/Program.cs
+++ seminar6/Program.cs
@@ -2,9 +2,9 @@
 {
   public static void Main(string[] args)
   {
-    // Console.WriteLine("Task 41 started\n");
-    // Task41.run();
-    // Console.WriteLine("\nTask 41 finished\n");
+    Console.WriteLine("Task 41 started\n");
+    Task41.run();
+    Console.WriteLine("\nTask 41 finished\n");
 
     Console.WriteLine("Task 43 started\n");
     Task43.run();
