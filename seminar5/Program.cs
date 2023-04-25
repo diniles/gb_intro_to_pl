6bd5cbf1ae21db4acf3608fff7f348b778464b15
diniles <dzliashko@gmail.com>
2023-04-25 14:44:12 +0300

    Feat: all tasks solved

--- seminar5/Program.cs
+++ seminar5/Program.cs
@@ -4,13 +4,13 @@ public class Program
 {
   public static void Main(string[] args)
   {
-    // Console.WriteLine("Task 34 started\n");
-    // Task34.run();
-    // Console.WriteLine("\nTask 34 finished\n");
+    Console.WriteLine("Task 34 started\n");
+    Task34.run();
+    Console.WriteLine("\nTask 34 finished\n");
 
-    // Console.WriteLine("Task 36 started\n");
-    // Task36.run();
-    // Console.WriteLine("\nTask 36 finished\n");
+    Console.WriteLine("Task 36 started\n");
+    Task36.run();
+    Console.WriteLine("\nTask 36 finished\n");
 
     Console.WriteLine("Task 38 started\n");
     Task38.run();
