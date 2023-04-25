cf72132efcfc17bbc8f18f93296ff241178b30e7
Dzmitry Liashko <dzliashko@gmail.com>
2023-04-25 21:41:25 +0300

    Feat: main file Program.cs

--- seminar6/Program.cs
+++ seminar6/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+  public static void Main(string[] args)
+  {
+    Console.WriteLine("Task 41 started\n");
+    Task41.run();
+    Console.WriteLine("\nTask 41 finished\n");
+
+    Console.WriteLine("Task 43 started\n");
+    Task43.run();
+    Console.WriteLine("\nTask 43 finished\n");
+  }
+}
