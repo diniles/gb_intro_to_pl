6fd06143dd2701ef5ff835d940d3e973b9818680
diniles <dzliashko@gmail.com>
2023-04-25 13:00:16 +0300

    Fix:errors

--- seminar5/common.cs
+++ seminar5/common.cs
@@ -1,39 +1,44 @@
 // common functions for all tasks
-int prompt(string prompt)
+
+public class Common
 {
-  Console.Write($"{prompt}: ");
-  string input = Console.ReadLine();
-  return int.Parse(input);
+  public int prompt(string prompt)
+  {
+    Console.Write($"{prompt}: ");
+    string input = Console.ReadLine();
+    return int.Parse(input);
+  }
+
+  public int[] fillArrayOfInts(int size, int min, int max)
+  {
+    int[] array = new int[size];
+    Random randomNumber = new Random();
+    for (int i = 0; i < size; i++)
+    {
+      array[i] = randomNumber.Next(min, max + 1);
+    }
+    return array;
+  }
+
+  public double[] fillArrayOfDoubles(int size)
+  {
+    double[] array = new double[size];
+    Random randomNumber = new Random();
+    for (int i = 0; i < size; i++)
+    {
+      array[i] = randomNumber.NextDouble();
+    }
+    return array;
+  }
+
+  public void printArray(int[] array)
+  {
+    Console.Write("[");
+    foreach (var item in array)
+    {
+      Console.Write($"{item}, ");
+    }
+    Console.Write("\b\b]");
+  }
 }
 
-int[] fillArrayOfInts(int size, int min, int max)
-{
-  int[] array = new int[size];
-  Random randomNumber = new Random();
-  for (int i = 0; i < size; i++)
-  {
-    array[i] = randomNumber.Next(min, max + 1);
-  }
-  return array;
-}
-
-double[] fillArrayOfDoubles(int size, double min, double max)
-{
-  double[] array = new double[size];
-  Random randomNumber = new Random();
-  for (int i = 0; i < size; i++)
-  {
-    array[i] = randomNumber.Next(min, max + 1);
-  }
-  return array;
-}
-
-void printArray(int[] array)
-{
-  Console.Write("[");
-  foreach (var item in array)
-  {
-    Console.Write($"{item}, ");
-  }
-  Console.Write("\b\b]");
-}
