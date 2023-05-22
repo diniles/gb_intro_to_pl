1036153ab3a3ae155fcaf9ae89f048d4f2eb4ba3
diniles <dzliashko@gmail.com>
2023-05-22 16:04:47 +0300

    Feat: seminar 8 Program.cs

--- seminar8/Program.cs
+++ seminar8/Program.cs
@@ -6,21 +6,21 @@
     Task54.run();
     Console.WriteLine("\nTask 54 finished\n");
 
-    Console.WriteLine("Task 56 started\n");
-    Task56.run();
-    Console.WriteLine("\nTask 56 finished\n");
+    // Console.WriteLine("Task 56 started\n");
+    // Task56.run();
+    // Console.WriteLine("\nTask 56 finished\n");
 
-    Console.WriteLine("Task 58 started\n");
-    Task58.run();
-    Console.WriteLine("\nTask 58 finished\n");
+    // Console.WriteLine("Task 58 started\n");
+    // Task58.run();
+    // Console.WriteLine("\nTask 58 finished\n");
 
-    Console.WriteLine("Task 60 started\n");
-    Task60.run();
-    Console.WriteLine("\nTask 60 finished\n");
+    // Console.WriteLine("Task 60 started\n");
+    // Task60.run();
+    // Console.WriteLine("\nTask 60 finished\n");
 
-    Console.WriteLine("Task 62 started\n");
-    Task62.run();
-    Console.WriteLine("\nTask 62 finished\n");
+    // Console.WriteLine("Task 62 started\n");
+    // Task62.run();
+    // Console.WriteLine("\nTask 62 finished\n");
 
   }
 }
