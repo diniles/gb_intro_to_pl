464179e12f50a0d0866ad53278dfd6e5dd04d309
Dzmitry Liashko <dzliashko@gmail.com>
2023-05-24 21:12:24 +0300

    Refactor: fill2DArrayOfInts in common lib

--- seminar8/Task62.cs
+++ seminar8/Task62.cs
@@ -8,6 +8,40 @@ public class Task62
 {
   public static void run()
   {
+    int size = Common.prompt("Enter matrix size");
 
+    int[,] matrix = fillMatrix(size);
+
+    Common.print2DArrayOfInts(matrix);
+  }
+
+
+  public static int[,] fillMatrix(int size)
+  {
+    int[,] matrix = new int[size, size];
+
+    int pos = 1;
+    int count = size;
+    int value = -size;
+    int sum = -1;
+
+    do
+    {
+      value = -1 * value / size;
+      for (int i = 0; i < count; i++)
+      {
+        sum += value;
+        matrix[sum / size, sum % size] = pos++;
+      }
+      value *= size;
+      count--;
+      for (int i = 0; i < count; i++)
+      {
+        sum += value;
+        matrix[sum / size, sum % size] = pos++;
+      }
+    } while (count > 0);
+
+    return matrix;
   }
 }
