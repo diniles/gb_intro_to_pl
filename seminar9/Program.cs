6c22111c4d9da706f1ff074cf54a46426ef44b3a
Dzmitry Liashko <dzliashko@gmail.com>
2023-05-24 21:42:00 +0300

    Feat: seminar 9 start task 66

--- seminar9/Program.cs
+++ seminar9/Program.cs
@@ -6,9 +6,9 @@
     // Task64.run();
     // Console.WriteLine("\nTask 64 finished\n");
 
-    // Console.WriteLine("Task 66 started\n");
-    // Task66.run();
-    // Console.WriteLine("\nTask 66 finished\n");
+    Console.WriteLine("Task 66 started\n");
+    Task66.run();
+    Console.WriteLine("\nTask 66 finished\n");
 
     // Console.WriteLine("Task 68 started\n");
     // Task68.run();
