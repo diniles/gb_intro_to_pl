07a312275e59b8c3b2e3469b132482a973ef3fa9
diniles <dzliashko@gmail.com>
2023-05-24 16:15:46 +0300

    Refactor: seminar 8 task 62 started

--- seminar8/Program.cs
+++ seminar8/Program.cs
@@ -14,13 +14,13 @@
     // Task58.run();
     // Console.WriteLine("\nTask 58 finished\n");
 
-    Console.WriteLine("Task 60 started\n");
-    Task60.run();
-    Console.WriteLine("\nTask 60 finished\n");
+    // Console.WriteLine("Task 60 started\n");
+    // Task60.run();
+    // Console.WriteLine("\nTask 60 finished\n");
 
-    // Console.WriteLine("Task 62 started\n");
-    // Task62.run();
-    // Console.WriteLine("\nTask 62 finished\n");
+    Console.WriteLine("Task 62 started\n");
+    Task62.run();
+    Console.WriteLine("\nTask 62 finished\n");
 
   }
 }
