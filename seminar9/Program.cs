94264b3bd3b789a54f98a5b8bc7d67c380251c2a
Dzmitry Liashko <dzliashko@gmail.com>
2023-05-24 21:37:04 +0300

    Init: seminar 9 start

---
+++ seminar9/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Program
+{
+  public static void Main(string[] args)
+  {
+    // Console.WriteLine("Task 64 started\n");
+    // Task64.run();
+    // Console.WriteLine("\nTask 64 finished\n");
+
+    // Console.WriteLine("Task 66 started\n");
+    // Task66.run();
+    // Console.WriteLine("\nTask 66 finished\n");
+
+    // Console.WriteLine("Task 68 started\n");
+    // Task68.run();
+    // Console.WriteLine("\nTask 68 finished\n");
+  }
+}
