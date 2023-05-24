168bc080896a1a1369a5ef27b1d53e67157d0b0b
diniles <dzliashko@gmail.com>
2023-05-24 13:01:26 +0300

    Feat: seminar 8 task 56 solved

--- seminar8/Task56.cs
+++ seminar8/Task56.cs
@@ -17,6 +17,35 @@ public class Task56
 {
   public static void run()
   {
+    int rows = Common.prompt("Enter number of rows");
+    int cols = Common.prompt("Enter number of columns");
 
+    int[][] jaggedArray = Common.fillJaggedArrayOfInts(rows, cols, 0, 99);
+
+    Common.printJaggedArrayOfInts(jaggedArray);
+
+    int minSum = Int32.MaxValue;
+    int minRow = 0;
+
+    for (int i = 0; i < jaggedArray.Length; i++)
+    {
+      int sum = sumOfElements(jaggedArray[i]);
+      if (minSum > sum)
+      {
+        minSum = sum;
+        minRow = i;
+      }
+    }
+    Console.WriteLine($"{minRow + 1} line");
+  }
+
+  public static int sumOfElements(int[] jArray)
+  {
+    int sum = 0;
+    foreach (var el in jArray)
+    {
+      sum += el;
+    }
+    return sum;
   }
 }
