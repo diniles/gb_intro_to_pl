001d8ba6d994d8109b26121d0a4f8ad16138781e
diniles <dzliashko@gmail.com>
2023-05-25 09:25:21 +0300

    Feat: seminar 9 task 66 solved

--- seminar9/Task66.cs
+++ seminar9/Task66.cs
@@ -7,6 +7,31 @@ public class Task66
 {
   public static void run()
   {
+    int m = prompt("Enter the start number");
+    int n = prompt("Enter the end number");
 
+    Console.WriteLine(sumOfNaturals(m, n));
+  }
+
+  public static int sumOfNaturals(int m, int n)
+  {
+    if (m == n)
+    {
+      return m;
+    }
+    else
+    {
+      return m + sumOfNaturals(m + 1, n);
+    }
+  }
+  public static int prompt(string prompt)
+  {
+    Console.Write($"{prompt} (if empty - 0): ");
+    string? input = Console.ReadLine();
+    if (input == null || input == "")
+    {
+      input = "0";
+    }
+    return int.Parse(input);
   }
 }
