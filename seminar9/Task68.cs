90d0efa2c5e3b0b7ddf27a6eb8dccf8402b0f365
diniles <dzliashko@gmail.com>
2023-05-25 10:03:55 +0300

    Feat: seminar 9 task 68 solved

--- seminar9/Task68.cs
+++ seminar9/Task68.cs
@@ -3,10 +3,40 @@
 // m = 2, n = 3->A(m, n) = 9
 // m = 3, n = 2->A(m, n) = 29
 
-public class Task64
+public class Task68
 {
   public static void run()
   {
+    int m = prompt("Enter the start number");
+    int n = prompt("Enter the end number");
 
+    Console.WriteLine(ackermann(m, n));
+  }
+
+  public static int ackermann(int m, int n)
+  {
+    if (m == 0)
+    {
+      return n + 1;
+    }
+    else if (n == 0)
+    {
+      return ackermann(m - 1, 1);
+    }
+    else
+    {
+      return ackermann(m - 1, ackermann(m, n - 1));
+    }
+  }
+
+  public static int prompt(string prompt)
+  {
+    Console.Write($"{prompt} (if empty - 0): ");
+    string? input = Console.ReadLine();
+    if (input == null || input == "")
+    {
+      input = "0";
+    }
+    return int.Parse(input);
   }
 }
